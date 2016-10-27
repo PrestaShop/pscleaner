889dfdac3932a923b808809f38f21f72ff695989
Maxime Biloé <maxime.biloe@prestashop.com>
2016-10-27 17:40:42 +0200

    // Version updated to v1.9.1

--- pscleaner.php
+++ pscleaner.php
@@ -35,7 +35,7 @@ class PSCleaner extends Module
     {
         $this->name = 'pscleaner';
         $this->tab = 'administration';
-        $this->version = '1.9.0';
+        $this->version = '1.9.1';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
         if (version_compare(_PS_VERSION_, '1.5.0.0 ', '>=')) {
